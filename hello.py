dbcf73d252a8525adfd9cf3b79f763434fbd48c9
tiktak2014 <tiktak_2014@163.com>
2014-10-22 17:15:51 +0800

    use flask extensions: flask-bootstrap, flask-moment

--- hello.py
+++ hello.py
@@ -2,13 +2,21 @@
 
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route("/<name>")
-def index(name):
-    return render_template("hello.html", name=name)
+@app.route("/")
+def index():
+    name = "steven"
+    from datetime import datetime
+    return render_template("hello.html",
+                           name=name,
+                           current_time=datetime.utcnow())
 
 
 if __name__ == "__main__":
