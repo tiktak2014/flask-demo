10e331edd81b3c1b020b8ebb1ed82c3d695bf9c1
tiktak2014 <tiktak_2014@163.com>
2014-10-22 15:19:11 +0800

    HelloWorld

---
+++ hello.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello, Flask!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
