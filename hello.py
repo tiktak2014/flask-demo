0cf189a46db03653471c60cc9463bd338491c85b
tiktak2014 <tiktak_2014@163.com>
2014-10-22 15:32:16 +0800

    @app.route(): dynamic uri (integer & float & path)

--- hello.py
+++ hello.py
@@ -10,5 +10,25 @@ def index():
     return "<h1>Hello, Flask!</h1>"
 
 
+@app.route("/user/<name>")
+def user_name(name):
+    return "user_name<br /><h1>Hello, %s!</h1>" % name
+
+
+@app.route("/user/<int:id>")
+def user_id(id):
+    return "user_id<br /><h1>Hello, %s!</h1>" % id
+
+
+@app.route("/goods/<float:total>")
+def goods_total(total):
+    return "goods_total<br /><h1>Hello, %s!</h1>" % total
+
+
+@app.route("/user/<path:uri>")
+def user_uri(uri):
+    return "user_uri<br /><h1>Hello, %s!</h1>" % uri
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
