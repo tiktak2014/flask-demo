4a50d4e4579ab7440b75a113230b126d2a657344
tiktak2014 <tiktak_2014@163.com>
2014-10-24 01:04:59 +0800

    remove flask-bootstrap extension

--- hello.py
+++ hello.py
@@ -2,10 +2,8 @@
 
 from flask import Flask, render_template
 from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 
 @app.route("/")
